0085db8bd73f6e3ceee040eba57cdea3b77955fd
Shivam Agrahari <135615585+shivamagrhri@users.noreply.github.com>
2024-06-10 23:57:49 +0530

    Add files via upload
    This is C programming form basic to advance

---
+++ Day 1/First C Program/print.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+int main()
+{
+  printf("Hello World\n");
+  printf("My age is %d",32);
+  return 0;
+}  
---
+++ Day 1/Installation/hello.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main()
+{
+  printf("Shivam Agrahari");
+  return 0;
+}
